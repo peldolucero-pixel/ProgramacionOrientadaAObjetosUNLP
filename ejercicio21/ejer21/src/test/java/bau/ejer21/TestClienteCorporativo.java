c43456fb8911fdebf5bdaf6645c569974764dbee
peldolucero@gmail.com <peldolucero@gmail.com>
2025-11-04 11:20:09 -0300

    termine

--- ejercicio21/ejer21/src/test/java/bau/ejer21/TestClienteCorporativo.java
+++ ejercicio21/ejer21/src/test/java/bau/ejer21/TestClienteCorporativo.java
@@ -1,16 +1,32 @@
 package bau.ejer21;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 public class TestClienteCorporativo {
 	Cliente cliente;
+	
 	Envio envio1;
 	Envio envio2;
 	Envio envio3;
-	
 	@BeforeEach
 	void setUp() throws Exception{
-		cliente = new ClienteCorporativo("juanAsociados", "32 N°112",2323322);
+		cliente = new ClienteCorporativo("pedro", "pedro@hotmail.com", 23232323);
+		Envio envio1 = new EnvioLocal(LocalDate.of(2025, 10, 15),"formosa", "la pampa", 500);
+		Envio envio2 = new EnvioInterurbano(LocalDate.of(2025,11,1),"formosa", "la pampa", 50,10);
+		Envio envio3 = new EnvioInternacional(LocalDate.of(2025,11,3),"formosa", "la pampa", 10);
+		cliente.agregarEnvio(envio1);
+		cliente.agregarEnvio(envio2);
+		cliente.agregarEnvio(envio3);
 	}
 	
+	
+	@Test
+	public void testCalcularCosto() {
+		assertEquals(cliente.calcularMontoEnPerido(LocalDate.of(2025,10,15), LocalDate.of(2025, 11, 1)),2000);
+		assertEquals(cliente.calcularMontoEnPerido(LocalDate.of(2025,10,1), LocalDate.of(2025, 10, 2)),0);
+		
+	}
 }
